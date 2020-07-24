df88a557ec4abae88a29a5a81dde107f2818e98e
Robert Luttig <robert.luttig@gmail.com>
2020-07-24 15:13:25 -0700

    added recipe db to models

--- models/recipeDB.js
+++ models/recipeDB.js
@@ -1,25 +1,23 @@
 module.exports = function(sequelize, DataTypes) {
-    const Recipe = sequelize.define("Recipe", {
-      RecipeLabel: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          len: [1]
-        }
-      },
-      Image: {
-        type: DataTypes.STRING
-      },
-      PrepTime: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          len: [1]
-        }
+  const Recipe = sequelize.define("Recipe", {
+    RecipeLabel: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
       }
-    });
-  
-    return Recipe;
-  };
+    },
+    Image: {
+      type: DataTypes.STRING
+    },
+    PrepTime: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    }
+  });
 
- 
+  return Recipe;
+};
