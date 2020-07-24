11af1ca3ca355709216127a7f2179648687e2428
nhuanthi <tlam1288@gmail.com>
2020-07-24 13:36:29 -0700

    added recipe.js in routes which handles the users search query to call the api. added search.js in /public to handle the button click event to get the users search query

---
+++ public/js/search.js
@@ -0,0 +1,26 @@
+//const express = require("express");
+
+$("#submit").on("click", (event) => {
+  event.preventDefault();
+
+  const newQuery = {
+    query: $("#search")
+      .val()
+      .trim(),
+  };
+  console.log(newQuery);
+
+  $.ajax("/search", {
+    type: "GET",
+    data: newQuery,
+  }).then(() => {
+    console.log(newQuery);
+    //location.reload();
+  });
+
+  // $.post("/api/recipe", newQuery).then((data) => {
+  //   console.log(data);
+
+  //   alert("Searching...");
+  // });
+});
