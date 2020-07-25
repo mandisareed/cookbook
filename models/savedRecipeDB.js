23ee610a92f0757fbee61567e510d437015bb371
Robert Luttig <robert.luttig@gmail.com>
2020-07-25 16:07:36 -0700

    created saved recipe DB and association in user DB

---
+++ models/savedRecipeDB.js
@@ -0,0 +1,29 @@
+module.exports = function(sequelize, DataTypes) {
+  const SavedRecipe = sequelize.define("SavedRecipe", {
+    RecipeLabel: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    Image: {
+      type: DataTypes.STRING
+    },
+    PrepTime: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    }
+  });
+  SavedRecipe.associate = function(models) {
+    SavedRecipe.belongsTo(models.User, {
+      foreignKey: {
+        allowNull: false
+      }
+    });
+  };
+  return SavedRecipe;
+};
