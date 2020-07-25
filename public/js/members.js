35d8b3804a5e00190440c6754d56d56383a6bdcd
lindyem <lindy.merwin@gmail.com>
2020-07-24 17:23:33 -0700

    Cards for Query resuts

--- public/js/members.js
+++ public/js/members.js
@@ -1,7 +1,33 @@
 $(document).ready(() => {
   // This file just does a GET request to figure out which user is logged in
   // and updates the HTML on the page
-  $.get("/api/user_data").then(data => {
-    $(".member-name").text(data.email);
+  var searchBtn = $(".searchBtn");
+
+  searchBtn.on("click", (event) => {
+    var appId = "add72012";
+    var appKey = "cb87396ddb413723f833ddcae1c1d4e5";
+    var searchQuery = $("#searchInput").val();
+
+    $.get(
+      `https://api.edamam.com/search?q=${searchQuery}&app_id=${appId}&app_key=${appKey}`,
+      function(response) {
+        console.log("response", response);
+        var hits = response.hits;
+
+        for (var i = 0; i < hits.length; i++) {
+          var recipe = $("<div>");
+          recipe.attr("data-id", hits[i].recipe.uri);
+          recipe.html(`
+            <div class="card" style="width: 18rem;">
+              <img src="${hits[i].recipe.image}" class="card-img-left" alt="...">
+              <div class="card-body">
+                <p class="card-text">${hits[i].recipe.label}</p>
+              </div>
+            </div>
+          `);
+          $("#results").append(recipe);
+        }
+      }
+    );
   });
 });
