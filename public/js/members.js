76566e23bd5db786d158ccc0502621497c1db7bc
lindyem <lindy.merwin@gmail.com>
2020-07-25 15:10:42 -0700

    llint fix

--- public/js/members.js
+++ public/js/members.js
@@ -1,30 +1,32 @@
 $(document).ready(() => {
   // This file just does a GET request to figure out which user is logged in
   // and updates the HTML on the page
-  var searchBtn = $(".searchBtn");
+  const searchBtn = $(".searchBtn");
 
-  searchBtn.on("click", (event) => {
-    var appId = "add72012";
-    var appKey = "cb87396ddb413723f833ddcae1c1d4e5";
-    var searchQuery = $("#searchInput").val();
+  searchBtn.on("click", () => {
+    const appId = "add72012";
+    const appKey = "cb87396ddb413723f833ddcae1c1d4e5";
+    const searchQuery = $("#searchInput").val();
 
     $.get(
       `https://api.edamam.com/search?q=${searchQuery}&app_id=${appId}&app_key=${appKey}`,
-      function(response) {
+      response => {
         $("#results").empty();
         console.log("response", response);
-        var hits = response.hits;
-        
-        $("#results").append(`<div class="font-weight-bold">Results: ${hits.length}</div>`);
+        const hits = response.hits;
 
-        for (var i = 0; i < hits.length; i++) {
-          var recipe = $("<div>");
+        $("#results").append(
+          `<div class="font-weight-bold">Results: ${hits.length}</div>`
+        );
+
+        for (let i = 0; i < hits.length; i++) {
+          const recipe = $("<div>");
           recipe.attr("data-id", hits[i].recipe.uri);
           recipe.html(`
             <div class="card">
               <div class="card-body">
                 <img class="card-image" src="${hits[i].recipe.image}">
-                <div>
+                <div></div>
                   <div class="card-text">${hits[i].recipe.label}</div>
                 </div>
               </div>
