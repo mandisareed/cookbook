e34db264b4bf0237c8fb35310512372fb3319379
nhuanthi <tlam1288@gmail.com>
2020-07-26 15:41:14 -0700

    added more data-attributes to members.js and created route to save recipes with button click

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 // Requiring our models and passport as we've configured it
 const db = require("../models");
 const passport = require("../config/passport");
