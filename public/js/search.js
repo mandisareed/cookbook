f59ba65744e0305d2e09b81e43d946f5d480483d
Mandisa Reed <mandisa.reed@gmail.com>
2020-07-25 23:48:01 -0400

    added handlebards view to display saved searches with delete button

--- public/js/search.js
+++ public/js/search.js
@@ -25,3 +25,33 @@ $("#submit").on("click", (event) => {
   //   alert("Searching...");
   // });
 });
+
+
+//i don't think a delete function is needed in this file, as it's in the saved.handlebards file
+
+//delete function to delete a saved recipe
+//not sure which of this two functions is more in the right direction:
+// $(".delete-recipe").on("click", function () {
+//   const id = $(this).data("id");
+
+//   // Send the DELETE request.
+//   //what's the route to the saved recipes page
+//   $.ajax(`/api/saved/${id}`, {
+//     type: "DELETE",
+//   }).then(() => {
+//     // Reload the page to get the updated list
+//     location.reload();
+//   });
+// });
+
+// //OR
+
+// function deleteRecipe(id) {
+//   $.ajax({
+//     method: "DELETE",
+//     url: `/api/saved/${id}`
+//   })
+//     .then(() => {
+//       getPosts(postCategorySelect.val());
+//     });
+// }
