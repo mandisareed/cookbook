8a52e21872154f763766ac733fe10397d088e938
nhuanthi <tlam1288@gmail.com>
2020-07-28 11:31:23 -0700

    added script to drop database

---
+++ scripts/force-sync-db.js
@@ -0,0 +1,12 @@
+/* eslint-disable prettier/prettier */
+const db = require("../models");
+db.sequelize
+  .sync({ force: true })
+  .then(() => {
+    console.log("db was reset");
+    process.exit(0);
+  })
+  .catch((error) => {
+    console.log(error);
+    process.exit(1);
+  });
